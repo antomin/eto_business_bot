86a19a290e91daa52425267114f01af9865b40d3
antomin <anton.mindlin@gmail.com>
2023-01-13 18:24:53 +0100

    Admin services ready

--- service_app/admin.py
+++ service_app/admin.py
@@ -1,7 +1,44 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from service_app.models import ServiceCategory, ServiceSubCategory, ServiceUser
 
-admin.site.register(ServiceCategory)
-admin.site.register(ServiceSubCategory)
-admin.site.register(ServiceUser)
+
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_active')
+    list_display_links = ('title',)
+    search_fields = ('title',)
+    list_editable = ('is_active',)
+    list_filter = ('is_active',)
+
+
+class ServiceSubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'is_active')
+    list_display_links = ('title',)
+    search_fields = ('title',)
+    list_editable = ('is_active',)
+    list_filter = ('category', 'is_active')
+
+
+class ServiceUserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'created_at', 'updated_at', 'is_top', 'is_active')
+    list_display_links = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
+    list_editable = ('is_top', 'is_active')
+    list_filter = ('is_top', 'is_active')
+    fields = ('first_name', 'last_name', 'sub_category', 'description', 'image_url', 'email', 'tg', 'is_top',
+              'is_active', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+
+    # def get_html_photo(self, object):
+    #     if object.image_url:
+    #         return mark_safe(f'<img src="{object.image_url}" width=100')
+
+
+admin.site.register(ServiceCategory, ServiceCategoryAdmin)
+admin.site.register(ServiceSubCategory, ServiceSubCategoryAdmin)
+admin.site.register(ServiceUser, ServiceUserAdmin)
+
+admin.site.site_title = '@eto_business_bot'
+admin.site.site_header = 'Администрирование @eto_business_bot'
+admin.site.site_url = None
--- service_app/models.py
+++ service_app/models.py
@@ -15,6 +15,7 @@ class ServiceCategory(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+        ordering = ['-is_active', 'title']
 
 
 class ServiceSubCategory(models.Model):
@@ -32,21 +33,24 @@ class ServiceSubCategory(models.Model):
     class Meta:
         verbose_name = 'Подкатегория'
         verbose_name_plural = 'Подкатегории'
+        ordering = ['-is_active', 'title']
 
 
 class ServiceUser(models.Model):
     first_name = models.CharField(verbose_name='имя', max_length=24)
     last_name = models.CharField(verbose_name='фамилия', max_length=24)
-    image_url = models.ImageField(upload_to='service_images', verbose_name='изображение', max_length=64)
+    image_url = models.ImageField(upload_to='service_img', verbose_name='изображение', max_length=64, blank=True)
     sub_category = models.ManyToManyField(ServiceSubCategory, verbose_name='категории')
     description = models.TextField(verbose_name='описание')
     email = models.EmailField(verbose_name='email', blank=True, unique=True)
     tg = models.CharField(verbose_name='телеграм', max_length=24, blank=True, unique=True)
     is_top = models.BooleanField(verbose_name='топ', default=False)
     is_active = models.BooleanField(verbose_name='активна', default=True)
+    created_at = models.DateTimeField(verbose_name='время добавления', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='последнее обновление', auto_now=True)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name} ({self.sub_category.first()})'
+        return f'{self.first_name} {self.last_name}'
 
     def delete(self, using=None, keep_parents=False):
         self.is_active = False
@@ -55,3 +59,4 @@ class ServiceUser(models.Model):
     class Meta:
         verbose_name = 'Услуга'
         verbose_name_plural = 'Услуги'
+        ordering = ['-is_active', 'updated_at']
